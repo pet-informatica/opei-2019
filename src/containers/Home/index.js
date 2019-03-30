db7fff448c794244f12d7e1da4a3abecd8295bce
Henrique Caúla <lhtc@cin.ufpe.br>
2019-03-30 19:58:44 -0300

    Creates project basic structure

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Landing from './Landing'
+import About from './About'
+import './Home.css'
 
 export default class Home extends Component {
   render() {
     return (
-      <div> Home </div>
+      <div className="Home">
+        <Landing />
+        <About />
+      </div>
     )
   }
 }

7ba782e4721007a3b19ae2f448fcf352537bb54e
Eden Santana <eeas@cin.ufpe.br>
2019-03-31 17:09:04 -0300

    Embeded Lists to Footer and Columns Components

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import List from '../List/List.js';
 
 import './Columns.css'
 
 export default class Columns extends Component {
   render() {
+    const {data} = this.props;
+    
     return (
       <div className="Columns">
         <h3> {this.props.title} </h3>
+        <div className="List">
+          <List items={data}/>
+        </div>
       </div>
     )
   }
--- src/containers/Home/Footer/Footer.js
+++ src/containers/Home/Footer/Footer.js
@@ -6,15 +6,36 @@ import './Footer.css'
 
 export default class Footer extends Component {
   render() {
+    const links_opei = [
+      {"name": "Home", "route": "/"},
+      {"name": "Sobre", "route": "/About"},
+      {"name": "Contato", "route": "/Contact"}
+    ]
+
+    const links_provas_anteriores = [
+      {"name": "2015", "route": "/"},
+      {"name": "2016", "route": "/"},
+      {"name": "2017", "route": "/"},
+      {"name": "2018", "route": "/"}
+    ]
+
+    const links_informacoes = [
+      {"name": "Perguntas Frequentes", "route": "/"},
+      {"name": "Divulgue", "route": "/"},
+      {"name": "Regulamento", "route": "/"}
+    ]
+
     return (
       <div className="Footer">
       <div className="Footer-content">
-        <Columns title="OPEI"/>
-        <Columns title="Provas Anteriores"/>
-        <Columns title="Informações"/>
+        <Columns title="OPEI" data={links_opei} />
+        <Columns title="Provas Anteriores" data={links_provas_anteriores}/>
+        <Columns title="Informações" data={links_informacoes}/>
       </div>
       <div className="Footer-contact">
-          <Columns title="Fale Conosco" />
+        <div className="Footer-contact-box">
+          <Columns title="Fale Conosco" data={links_opei} />
+        </div>
       </div>
       </div>
     )

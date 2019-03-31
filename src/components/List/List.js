03903b44c81b3c3cf1a71b3847e0ff7b65f35970
Eden Santana <eeas@cin.ufpe.br>
2019-03-31 17:08:07 -0300

    Created Lists Component

---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './List.css'
+
+export default class List extends Component {
+  render() {
+    const {items} = this.props;
+
+    const nameList = items.map(item => {
+      return (
+        <li> 
+            <a href={item.route}> {item.name} </a>
+        </li>
+      )
+    })
+    
+    return (
+      <div className="Items">
+        <ul>{nameList}</ul>
+      </div>
+    )
+  }
+}

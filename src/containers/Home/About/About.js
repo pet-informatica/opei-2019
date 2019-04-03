ae2450b6d21ee72969d450bef458bfab019b9cb9
Eden Santana <eeas@cin.ufpe.br>
2019-04-03 18:22:56 -0300

    Changes in About component

--- src/containers/Home/About/About.js
+++ src/containers/Home/About/About.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import Title from '../../../components/Title/Title';
+import {Responsive, Segment, Image} from 'semantic-ui-react';
 import './About.css'
 
 export default class About extends Component {
   render() {
    
     return (
-      <div className="About">
-          <div className = "About-image"></div>
-          <div className="About-content">
-            <Title name="Sobre" />
-            <div className="About-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id nunc non dolor malesuada venenatis.
+          <div className="About-wrapper"> 
+              <Responsive as={Segment} basic minWidth={992}>
+              <div className="About-image">
+                    <Image src='https://res.cloudinary.com/dkbuneg9h/image/upload/v1554168587/Clypeasteroida_ahtawr.svg' size="massive">
+                    </Image>
+              </div>
+              </Responsive>
+              <Responsive as={Segment} basic >
+                <div className="About-content-wrapper">
+                    
+                      <Title name="Sobre"/>
+                   
+                    
+                      <Segment basic className="About-segment" className="Segment-teste">
+                        <div>
+                            A Olimpíada Pernambucana de Informática (OPEI) é uma competição para os estudantes de Ensinos Fundamental 
+                            e Médio das instituições públicas e privadas de todo o estado de Pernambuco. 
+                            <br />
+                            <br />
+                        </div>
+                        <div>
+                          <a href="">Perguntas Frequentes, Modalidades e mais</a>
+                        </div>
+                      </Segment>
+                    </div>
                
-                <a href="/About">perguntas frequentes, modalidades e mais</a>
+              </Responsive>
             </div>
-          </div>
-      </div>
     )
   }
 }

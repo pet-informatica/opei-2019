7e4394bf3b4759f493e0c0ed246bd3d01d67ea80
Eden Santana <eeas@cin.ufpe.br>
2019-04-05 00:53:07 -0300

    final changes in About component

--- src/containers/Home/About/About.js
+++ src/containers/Home/About/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../../../components/Title/Title';
 import {Responsive, Segment, Image} from 'semantic-ui-react';
+import YouTube from 'react-youtube';
 import { HashLink as Link } from 'react-router-hash-link';
 import './About.css'
 
@@ -8,25 +9,36 @@ export default class About extends Component {
   render() {
     return (
           <div className="About-wrapper"> 
-              <Responsive as={Segment} basic minWidth={992}>
-                <div className="About-image">
-                      <Image src='https://res.cloudinary.com/dkbuneg9h/image/upload/v1554168587/Clypeasteroida_ahtawr.svg' size="massive">
-                      </Image>
-                </div>
-              </Responsive>
               <Responsive as={Segment} basic >
-                <div className="About-content-wrapper">    
-                      <Title name="Sobre"/>
-                      <Segment basic className="About-segment" className="Segment-teste">
-                            A Olimpíada Pernambucana de Informática (OPEI) é uma competição para os estudantes de Ensinos Fundamental 
-                            e Médio das instituições públicas e privadas de todo o estado de Pernambuco. 
-                            <br/>
-                            <br/>
-                            <Link to="/About#perguntas-frequentes">perguntas frequentes</Link>
-                            <a href="/About">modalidades e mais</a>
-                      </Segment>
+                <div className="About-content-wrapper">  
+
+                <div className="About-text">
+                  <Title name="Sobre"/>
+                  <Segment basic className="About-segment" className="Segment-teste">
+                  <div className="About-self-text">
+                    <p>
+                        A Olimpíada Pernambucana de Informática, OPEI, é uma competição para os estudantes de Ensinos Fundamental, 
+                        Médio e Superior das instituições públicas e privadas de todo o estado de Pernambuco. 
+                    </p>
+                       
+                  </div>
+                        <br/>
+                        <Link to="/About">
+                          <p>
+                            perguntas frequentes, modalidades e mais
+                          </p>
+                          </Link>
+                  </Segment>
+                </div>  
                 </div>
               </Responsive>
+              
+              <Responsive  basic minWidth={992}>
+                <div className="About-image">
+                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554428363/try4_j0so6j.png" size="large"/>
+                </div>
+              </Responsive>
+             
             </div>
     )
   }

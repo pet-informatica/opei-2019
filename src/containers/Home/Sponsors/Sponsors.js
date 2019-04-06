5e843c34774891f027cede0fcf9bb18c3a3b909c
Borba <mlbas@cin.ufpe.br>
2019-04-05 22:29:24 -0300

    Sponsers working, not yet completed

--- src/containers/Home/Sponsors/Sponsors.js
+++ src/containers/Home/Sponsors/Sponsors.js
@@ -1,46 +1,33 @@
 import React, { Component } from 'react';
 import Title from '../../../components/Title/Title';
-import { Image, Grid } from 'semantic-ui-react';
-import logo from '../../../assets/logo.svg';
+import { Image, Grid, GridColumn } from 'semantic-ui-react';
 
 export default class Sponsors extends Component {
     render() {
         
         return (
-            <div className="Home">
-                <Title name= "Realização"/>
-                <Grid stackable columns={1} id="Sponsors-Realizacao">
-                    <Grid.Column>
-                        <Image src={logo} size='medium'/>
-                    </Grid.Column>
-                </Grid>
-                <Title name= "Apoio"/>
-                <Grid stackable columns={3} id="Sponsors-Apoio">
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                </Grid>
-                <Title name= "Patrocinadores"/>
-                <Grid stackable columns={4} id="Sponsors-Patrocinio">
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image src={logo} size='small'/>
-                        </Grid.Column>
-                </Grid>
+            <div className="Sponsors">
+                <div className="Sponsors-Cards">
+                    <Grid stackable columns={7}>
+                        <Grid.Row stretched>
+                            <Grid.Column></Grid.Column>
+                            <Grid.Column>
+                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506920/Clypeasteroida_k7h4kl.svg" size='small'/>
+                            </Grid.Column>
+                            <Grid.Column></Grid.Column>
+                            <Grid.Column>
+                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506680/Logo_CIn-UFPE_rj1cnd.svg" size='small'/>
+                            </Grid.Column>
+                            <Grid.Column></Grid.Column>
+                            <Grid.Column>
+                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506899/LogoUFPE-Versoes_3_fq99zn.svg" size='small'/>
+                            {/* alterar imagem pra ajeitar altura top e bottom */}
+                            </Grid.Column>
+                            <Grid.Column></Grid.Column>
+                        </Grid.Row>
+                        <br/>
+                    </Grid>
+                </div>    
             </div>    
         );
     }

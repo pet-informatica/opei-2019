25e1a14605cb16a200c3e411e34380262b852865
Eden Santana <eeas@cin.ufpe.br>
2019-04-09 00:05:21 -0300

    Changed images in sponsors

--- src/containers/Home/Sponsors/Sponsors.js
+++ src/containers/Home/Sponsors/Sponsors.js
@@ -8,18 +8,18 @@ export default class Sponsors extends Component {
             <div className="Sponsors">
                 <div className="Sponsors-Cards">
                     <Grid stackable columns={7}>
-                        <Grid.Row stretched>
+                        <Grid.Row>
                             <Grid.Column></Grid.Column>
                             <Grid.Column>
                                 <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506920/Clypeasteroida_k7h4kl.svg" size='small'/>
                             </Grid.Column>
                             <Grid.Column></Grid.Column>
                             <Grid.Column>
-                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506680/Logo_CIn-UFPE_rj1cnd.svg" size='small'/>
+                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554778916/Layer_1_100x-8_ifbmmo.png" size='small'/>
                             </Grid.Column>
                             <Grid.Column></Grid.Column>
                             <Grid.Column>
-                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554506899/LogoUFPE-Versoes_3_fq99zn.svg" size='small'/>
+                                <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554645029/Layer_1_100x-8_ekpedk.png" size='small'/>
                             {/* alterar imagem pra ajeitar altura top e bottom */}
                             </Grid.Column>
                             <Grid.Column></Grid.Column>

00a7bc9408692fd6f478fe0dd8e62d92b911767c
Eden Santana <eeas@cin.ufpe.br>
2019-04-08 23:07:30 -0300

    Added image

--- src/containers/About/History/History.js
+++ src/containers/About/History/History.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Grid, Icon, Header, Card, Responsive} from 'semantic-ui-react';
+import {Grid, Icon, Header, Card, Responsive, Image} from 'semantic-ui-react';
 import './History.css';
 
 export default class History extends Component{
@@ -21,31 +21,40 @@ export default class History extends Component{
                     
                     <div className="History-cards-wrapper">
 						<Responsive minWidth='992'>
-							<Grid columns={5} divided='vertically'>
-								<Grid.Row >
-									<Grid.Column textAlign='center' className='history-items' >
-										<Icon name="flag checkered" size="huge" flipped='horizontally'></Icon>
-									</Grid.Column>
-									<Grid.Column/>
-									<Grid.Column textAlign='center'>
-										<Card color='green' header='2016' meta='21 escolas' description='224 alunos'/>
-									</Grid.Column>
-									<Grid.Column/>
-									<Grid.Column textAlign='center'>
-										<Card color='red' header='2018' meta='35 escolas' description='648 alunos'/>
-									</Grid.Column>
-								</Grid.Row>
-								<Grid.Row >
-									<Grid.Column/>
-									<Grid.Column textAlign='center'>
-										<Card color='blue' header='2015' meta='11 escolas' description='75 alunos'/>
-									</Grid.Column>
-									<Grid.Column/>
-									<Grid.Column textAlign='center'>
-										<Card color='yellow' header='2017' meta='22 escolas' description='516 alunos'/>								</Grid.Column>
-									<Grid.Column/>
-								</Grid.Row>
-							</Grid>
+						<div className="History-grid-image">
+							<div className="History-grid">
+								<Grid columns={5} divided='vertically'>
+									<Grid.Row >
+										<Grid.Column textAlign='center' className='history-items' >
+											<Icon name="flag checkered" size="huge" flipped='horizontally'></Icon>
+										</Grid.Column>
+										<Grid.Column/>
+										<Grid.Column textAlign='center'>
+											<Card color='green' header='2016' meta='21 escolas' description='224 alunos'/>
+										</Grid.Column>
+										<Grid.Column/>
+										<Grid.Column textAlign='center'>
+											<Card color='red' header='2018' meta='35 escolas' description='648 alunos'/>
+										</Grid.Column>
+									</Grid.Row>
+									<Grid.Row >
+										<Grid.Column/>
+										<Grid.Column textAlign='center'>
+											<Card color='blue' header='2015' meta='11 escolas' description='75 alunos'/>
+										</Grid.Column>
+										<Grid.Column/>
+										<Grid.Column textAlign='center'>
+											<Card color='yellow' header='2017' meta='22 escolas' description='516 alunos'/>								</Grid.Column>
+										<Grid.Column/>
+									</Grid.Row>
+								</Grid>
+							</div>
+							<div className="History-image">
+								<Responsive  basic minWidth={992}>
+									<Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1554428363/try4_j0so6j.png" size="medium"/>				
+								</Responsive>
+							</div>
+						</div>
 						</Responsive>
 						<Responsive maxWidth='991'>
 						<Grid columns={1} divided='vertically'>
